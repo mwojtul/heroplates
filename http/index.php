a0d8b1e1a4d28a0afe2d0068bac60af33b96f98c
Mark Wojtul <markwojtul@gmail.com>
2015-08-31 01:48:53 -0500

    svg attempt

--- http/index.php
+++ http/index.php
@@ -22,15 +22,24 @@
         <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
 
-        
-        <section class="heroplates">
-            <div class="heroplates__pane" style="background-image: url(img/heroplate-background_legacy-of-the-void.jpg)">
 
-            </div>
-            <div class="heroplates__pane heroplates__pane--bottom">
-                <div class="heroplates__pane-content" style="background-image: url(img/heroplate-background_world-of-warships.jpg)">
-                </div>
-            </div>
+        <section class="heroplates">
+            <div class="heroplates__pane">
+                <svg viewBox="0 0 200 90">
+                    <clipPath id="top">
+                        <polygon points="0,0 198,0 0,147 "/>
+                    </clipPath>
+                    <clipPath id="bottom">
+                        <polygon points="55,105 200,-5 200,90"/>
+                    </clipPath>
+                    <a id="pane-top" xlink:href="#" class="heroplates__link">
+                        <image clip-path="url(#top)" preserveAspectRatio="none" width="100%" height="100%" xlink:href="http://localhost/~mark/heroplates/http/img/heroplate-background_legacy-of-the-void.jpg"/>
+                    </a>
+                    <a id="pane-bottom" xlink:href="#" class="heroplates__link">
+                        <image clip-path="url(#bottom)" preserveAspectRatio="none" width="100%" height="100%" xlink:href="http://localhost/~mark/heroplates/http/img/heroplate-background_world-of-warships.jpg"/>
+                    </a>
+                </svg>
+            </div>           
         </section>
 
 
@@ -39,6 +48,7 @@
             window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')
         </script>
         <script src="js/plugins.js"></script>
+        <script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/1.15.0/TweenMax.min.js"></script>
         <script src="js/main.js"></script>
 
 
